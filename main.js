9067f6a0d68e01ecd03f036d6258f44538744f6f
princeV1411 <princevaishnav323@gmail.com>
2025-12-29 04:54:32 -0800

    login,pdf,js,functions add

---
+++ main.js
@@ -0,0 +1,10 @@
+let login =document.querySelector("#sig")
+    login.addEventListener("click",function(){
+        window.location.href = "other page/login page/index.html";
+});
+
+
+let sem1pdf = document.querySelector(".sem1");
+    sem1pdf.addEventListener("click",function(){
+        window.location.href = "src/sem1.pdf";
+});
